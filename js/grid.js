a2fd74ee1e4375c5b1246bdb84f38ed0f32a423c
Lsong <song940@gmail.com>
2015-06-10 23:10:59 +0800

    almost done .

--- js/grid.js
+++ js/grid.js
@@ -1,97 +1,156 @@
 ;(function(win){
 
+  /**
+   * [Grid]
+   * @param {[object]} options [配置参数]
+   */
   function Grid(options){
+    //default options
     this.options = {
-      prefix: 'tbl-',
+      data: [],
+      el: document.body,
       className: 'table'
     };
+    //overwrite default options
     for(var key in options){
       this.options[ key ] = options[ key ];
     }
-
+    //init options
+    this.el   = this.options.el;
     this.data = this.options.data;
+    this.initTable();
     return this;
   };
-
+  /**
+   * [renders 渲染器]
+   * @type {Object}
+   */
   Grid.renders = {
-    text: function(val){
-      return document.createTextNode(val);
+    button: function(val){
+      var btn = document.createElement('button');
+      btn.innerText = val;
+      return btn;
+    },
+    input: function(val){
+      var input = document.createElement('input');
+      input.value = val;
+      return input;
     }
   };
-
-
-  Grid.prototype.clear = function () {
-    this.options.el.innerHTML = '';
-  };
-
-  Grid.prototype.renderTable = function () {
+  /**
+   * [initTable create table struct]
+   * @return {[instance]} [this]
+   */
+  Grid.prototype.initTable = function () {
     this.table = document.createElement('table');
     this.table.className = this.options.className;
-    this.options.el.appendChild(this.table);
+    this.thead = document.createElement('thead');
+    this.tbody = document.createElement('tbody');
+    this.tfoot = document.createElement('tfoot');
+    this.table.appendChild(this.thead);
+    this.table.appendChild(this.tbody);
+    this.table.appendChild(this.tfoot);
+    this.el.appendChild(this.table);
     return this;
   };
-
+  /**
+   * [renderHeader description]
+   * @return {[type]} [description]
+   */
   Grid.prototype.renderHeader = function () {
-    var thead = document.createElement('thead');
-    var tr = document.createElement('tr');
     var colums = this.options.colums;
-    for(var key in colums){
-      var field = this.options.colums[ key ];
+    if(!colums && this.data.length > 0){
+      colums = [];
+      var model = this.data[ 0 ]; //1st.
+      for(var key in model){
+        colums.push({ name: key });
+      }
+      this.options.colums = colums;
+    }
+    var tr = document.createElement('tr');
+    for(var index in colums){
+      var field = colums[ index ];
       var th = document.createElement('th');
-      th.id = this.options.prefix + field.name;
-      th.innerText = field.label;
+      th.innerText = field.label || field.name;
       tr.appendChild(th);
     }
-    thead.appendChild(tr);
-    this.table.appendChild(thead);
+    this.thead.appendChild(tr);
   };
-
+  /**
+   * [renderColumns description]
+   * @param  {[type]} rowEl [description]
+   * @param  {[type]} model [description]
+   * @return {[type]}       [description]
+   */
   Grid.prototype.renderColumns = function (rowEl, model) {
-    for(var key in model){
-      var val = model[ key ];
-      var td = document.createElement('td');
-      var config = this.options.colums[ key ];
+    var colums = this.options.colums;
+    for(var index in colums){
+      var config = colums[index];
+      var val = model[ config.name ];
       var render = config.render;
       if(typeof render == 'string'){
-        render = Grid.renders[ config.render || 'text' ];
+        render = Grid.renders[ render ];
       }
-      td.appendChild( render(val) );
+      if(!render){
+        render = Grid.renders[ typeof val ];
+      }
+      if(!render){
+        render = function(val){
+          return document.createTextNode(val);
+        }
+      }
+      var td = document.createElement('td');
+      td.data = val;
+      td.appendChild(render(val || config.default || ''));
+      this.options.columRender && this.options.columRender(td);
       rowEl.appendChild(td);
     }
   };
-
-  Grid.createColumns = function(){
-    if(!this.options.colums){
-      this.options.colums = [];
-    }
-  };
-
+  /**
+   * [renderRows description]
+   * @return {[type]} [description]
+   */
   Grid.prototype.renderRows = function () {
-
-    var tbody = document.createElement('tbody');
-    for(var i=0;i<this.data.length;i++){
-      var item = this.data[i];
+    for(var index=0;index<this.data.length;index++){
+      var model = this.data[ index ];
       var tr = document.createElement('tr');
-      this.renderColumns(tr, item);
-      tbody.appendChild(tr);
+      tr.model = model;
+      this.renderColumns(tr, model);
+      this.options.rowRender && this.options.rowRender(tr);
+      this.tbody.appendChild(tr);
     }
-    this.table.appendChild(tbody);
   };
-
+  /**
+   * [clear description]
+   * @return {[type]} [description]
+   */
+  Grid.prototype.clear = function () {
+    this.thead.innerHTML = '';
+    this.tbody.innerHTML = '';
+    this.tfoot.innerHTML = '';
+  };
+  /**
+   * [renderFooter description]
+   * @return {[type]} [description]
+   */
   Grid.prototype.renderFooter = function () {
-
+    // this.tfoot.appendChild();
   };
-
-  Grid.prototype.render = function () {
+  /**
+   * [render description]
+   * @param  {[type]} data [description]
+   * @return {[type]}      [description]
+   */
+  Grid.prototype.render = function (data) {
+    if(data){
+      this.data = data;
+    }
     this.clear();
-    this.renderTable();
     this.renderHeader();
     this.renderRows();
     this.renderFooter();
   };
-
+  
   win.Grid = Grid;
-
-
 
 })(window);
